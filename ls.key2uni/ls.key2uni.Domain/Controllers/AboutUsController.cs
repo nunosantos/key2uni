b0a6e8db0882130d051a8efaa88f2cf68964102a
nunosantos <nuno.santos05@yahoo.com>
2013-09-15 11:10:46 +0100

    key2uni
    web application developed using ASP.NET MVC

---
+++ ls.key2uni/ls.key2uni.Domain/Controllers/AboutUsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ls.key2uni.Domain.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        //
+        // GET: /AboutUs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ls.key2uni/ls.key2uni.Domain/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ls.key2uni.Domain.MvcApplication" Language="C#" %>
